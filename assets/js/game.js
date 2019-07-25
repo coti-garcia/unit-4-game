10ccdc8cf1efe7ae8c8686145c252cf45af53288
coti_garcia <javiera.garcialatorre@gmail.com>
2019-07-24 22:00:10 -0400

    min max random values

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
     let wins = 0;
     let losses = 0;
-    let randomNum = Math.floor(Math.random() * 100);
-    let cristalOneValue = Math.floor(Math.random() * 10);
-    let cristalTwoValue = Math.floor(Math.random() * 10);
-    let cristalThreeValue = Math.floor(Math.random() * 10);
-    let cristalFourValue = Math.floor(Math.random() * 10);
+    let randomNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    let cristalOneValue = Math.floor((Math.random() * 12) + 1);
+    let cristalTwoValue = Math.floor((Math.random() * 12) + 1);
+    let cristalThreeValue = Math.floor((Math.random() * 12) + 1);
+    let cristalFourValue = Math.floor((Math.random() * 12) + 1);
     let userGuess = 0;
 
+    console.log(cristalOneValue);
+    console.log(cristalTwoValue);
+    console.log(cristalThreeValue);
+    console.log(cristalFourValue);
+    
     $("#wins").text(wins);
     $("#losses").text(losses);
     $("#random-num").text(randomNum);
+
+    
     
 });
